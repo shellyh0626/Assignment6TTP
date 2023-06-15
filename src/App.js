b1744f14ad3a218c396684ff19c50e00523f4986
Tashi <tashishyangba23@gmail.com>
2023-06-15 19:22:51 -0400

    added selector and Table class

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import { Table, TableRow, TableCell } from "./components";
 function App() {
   return (
     <div className="App">
-      
+      <select className='colorPicker'>
+        <option value="">default</option>
+        <option value="red">Red</option>
+        <option value="blue">Blue</option>
+        <option value="green">Green</option>
+        <option value="yello">Yellow</option>
+        <option value="pink">Pink</option>
+        <option value="white">White</option>
+      </select>
+      <Table/>
     </div>
   );
 }
