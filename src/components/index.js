9f0fd9e2f2019e7a079e7a63ce676676d72bd181
Rasul Nekzad <rasulnekzad@Rasuls-MacBook-Air.local>
2023-06-15 17:13:42 -0400

    initialized components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import App from '../App/App';
+import Table from '../Table/Table';
+import TableRow from '../TableRow/TableRow';
+import TableCell from '../TableCell/TableCell';
+
+export { App, Table, TableRow, TableCell };
