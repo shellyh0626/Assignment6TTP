af26c3077cba4a32b732755b44cef0d7f3f8ae2f
rasulnekzad <rasulnekzad@Rasuls-MacBook-Air.local>
2023-06-19 11:28:26 -0400

    redrafted components

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
 import TableCell from './TableCell/TableCell'
 
-class TableRow extends Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        cells: []
-      };
+const TableRow = ({numCols}) => {
+  const popCells = () => {
+    var cells = [];
+    for (let i = 0; i < numCols; i++) {
+      cells.push(<TableCell />);
+      return cells;
     }
-    
-    addNewCell = () => {
-      this.setState({
-        cells: [...this.state.cells, <TableCell key = {this.state.cells.length + 1} />]
-      })
-    }
-    
-    render() {
-    return (
-      <React.Fragment>
-      <tr>
-        {this.state.cells}
-      </tr>
-      </React.Fragment>
-    )
   }
+  
+  return (
+    <tr>{popCells()}</tr>
+  )
 }
 
 export default TableRow;
