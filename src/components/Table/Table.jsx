dd9f9d4906cdf7ed0a3e7d21bb1c51bb91761780
Tashi <tashishyangba23@gmail.com>
2023-06-19 12:45:37 -0400

    added remove function

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -7,7 +7,7 @@ const Table = () => {
   const [rows, setRows] = useState([]);
   const [numCols, setNumCols] = useState(1);
 
-  const addCell = () => {
+  const addColumn = () => {
     setNumCols(numCols+1);
   }
 
@@ -24,11 +24,25 @@ const Table = () => {
   const addRow = () => {
     setRows([...rows, <TableRow numCols={numCols} key={rows.length}/>]);
   }
-        
+
+  //removes a row from the grid utilizing the slice method
+  const removeRow = () =>{
+    setRows(rows.slice(0,-1));
+  }
+
+  //removes column from the grid as long as the numCol is more than 0
+  //so the program doesnt try to set the numCols to an negative number
+  const removeColumn = () =>{
+    if(numCols>0){
+      setNumCols(numCols-1);
+    }
+  }
   return (
     <div>
-        <button onClick={addCell}>Add Cell</button>
+        <button onClick={addColumn}>Add Column</button>
         <button onClick={addRow}>Add Row</button>
+        <button onClick={removeRow}>Remove Row</button>
+        <button onClick={removeColumn}>Remove Column</button>
       <table>
         <tbody>
           {rows}
