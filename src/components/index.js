dc1281ce2ee8f9d2f3401bcccc90577023c77fc1
Tashi <tashishyangba23@gmail.com>
2023-06-19 13:33:24 -0400

    removed dropdown component, and added select color function

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 //Barrel file for container components, which will be used to compose our main App component
-export {default as DropDownMenu} from './DropDownMenu/DropDownMenu';
 export {default as Table} from './Table/Table';
 export {default as TableRow} from './TableRow';
 export {default as TableCell} from './TableCell/TableCell';
